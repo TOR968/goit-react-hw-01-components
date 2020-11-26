e446ccbafb099a3fdd43aa3e404d6fe44104d5df
TOR968 <tor968@yahoo.com>
2020-11-26 20:45:54 +0200

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
-import Section from './components/section'
-import Profile from './components/profile/profile'
-import user from './components/profile/user.json'
-import StatisticsList from './components/statistics/statisticsList'
-import statisticalData from './components/statistics/statistical-data.json'
+import Section from './components/section';
+import Profile from './components/profile/profile';
+import user from './components/profile/user.json';
+import StatisticsList from './components/statistics/statisticsList';
+import statisticalData from './components/statistics/statistical-data.json';
+import FriendList from './components/friend-list/friendList';
+import friends from './components/friend-list/friends.json';
+
 
 function App() {
   return (
@@ -10,12 +13,11 @@ function App() {
       <Section title="Профиль социальной сети">
         <Profile {...user}/>
       </Section>
-
       <Section title="Секция статистики">
-         <StatisticsList stats={statisticalData} />,
+         <StatisticsList stats={statisticalData} />
       </Section>
       <Section title="Список друзей">
-
+        <FriendList friends={friends} />
       </Section>
       <Section title="История транзакций">
 
