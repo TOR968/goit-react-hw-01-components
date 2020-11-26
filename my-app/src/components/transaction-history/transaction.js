0aae7b4bcc7f4f7d782fbeda7ffc7f324a4d08cc
TOR968 <tor968@yahoo.com>
2020-11-26 21:13:25 +0200

    Update transaction.js

--- my-app/src/components/transaction-history/transaction.js
+++ my-app/src/components/transaction-history/transaction.js
@@ -3,28 +3,11 @@ import PropTypes from 'prop-types';
 
 function Transaction({type, amount, currency}) {
     return (
-<table className="transaction-history">
-  <thead>
     <tr>
-        <th>{type}</th>
-        <th>{amount}</th>
-        <th>{currency}</th>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
     </tr>
-  </thead>
-
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table>
     )
 }
  
