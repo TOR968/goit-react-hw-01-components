4a55975ccbf8f957a8278219cb389fe220dc84eb
TOR968 <tor968@yahoo.com>
2020-11-26 21:13:23 +0200

    Update transactionHistory.js

--- my-app/src/components/transaction-history/transactionHistory.js
+++ my-app/src/components/transaction-history/transactionHistory.js
@@ -3,15 +3,27 @@ import PropTypes from 'prop-types';
 
 function TransactionHistory({items}) {
     return (
-            items.map(item => (
-          <Transaction
-            key={item.id}
-            type={item.type}
-            amount={item.amount}
-            currency={item.currency}
-            />
-            ))
+        <table className="transaction-history">
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+            <tbody>
+                {items.map(item => (
+                     <Transaction
+                        key={item.id}
+                        type={item.type}
+                        amount={item.amount}
+                        currency={item.currency}
+                     />
+                ))}
+            </tbody>
+        </table>
     )
+    
 }
 
 TransactionHistory.propTypes = {
